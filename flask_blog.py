af89f2ae70bb9028960f25693f8a41a5d30ff58d
Khine <khinezar70122107@gmail.com>
2022-12-28 09:41:28 -1200

    register login manual

--- flask_blog.py
+++ flask_blog.py
@@ -1,17 +1,51 @@
-from flask import Flask
+from flask import Flask,render_template,url_for,flash,redirect
+from forms import RegistrationForm,LoginForm
 
 app = Flask(__name__)
 
-@app.route("/")
-@app.route("/home")
-def hello_world():
-    return "<p>Hello, World!</p>"
+app.config['SECRET_KEY'] = 'f0340dd98a079ace260952bec2bca1a8'
 
+posts = [
+    {
+        'author':'Corey Schafer',
+        'title':'Blog post 1',
+        'content':'First post content',
+        'date_posted':'April 20, 2018'
+    },
+    {
+        'author':'Johon Corner',
+        'title':'Blog post 2',
+        'content':'Second post content',
+        'date_posted':'April 21 , 2018'
+    }
+]
+
+@app.route("/")
+def home():
+    return render_template('home.html',posts=posts)
 
 @app.route("/about")
 def about():
-    return "<p>About Page!</p>"
+    return render_template('about.html',title='About')
 
-if __name__ == '__main__':
+@app.route("/register",methods=['GET','POST'])
+def register():
+    form=RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!','success')
+        return redirect(url_for('home'))
+    return render_template('register.html',title='Register',form=form)
+
+@app.route("/login",methods=['GET','POST'])
+def login():
+    form=LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@gmail.com' and form.password.data == 'password':
+            flash(f'You have been logged in!','success')
+            return redirect(url_for('home'))
+        else:
+            flash(f'Login Unsuccessful. Please check username and password!','danger')
+    return render_template('login.html',title='Login',form=form)
+
+if __name__=='__main__':
     app.run(debug=True)
-    #ဒါရေးပြီးရင် ဒီလို run လို့ရ python flask_blog.py
