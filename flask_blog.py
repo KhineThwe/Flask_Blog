020c5d1cd5f107fb761ea8560a8f6b642f7fdf41
Khine <khinezar70122107@gmail.com>
2022-12-28 13:58:40 -1200

    models and db

--- flask_blog.py
+++ flask_blog.py
@@ -1,9 +1,12 @@
 from flask import Flask,render_template,url_for,flash,redirect
 from forms import RegistrationForm,LoginForm
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-
 app.config['SECRET_KEY'] = 'f0340dd98a079ace260952bec2bca1a8'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+from models import User,Post
 
 posts = [
     {
