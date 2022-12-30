df6f313f4b27ebcb8eb618676a0690329d999de8
Khine <khinezar70122107@gmail.com>
2022-12-29 22:49:25 -1200

    CRUD post

--- flask_blog/routes.py
+++ flask_blog/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template,url_for,flash,redirect,request
+from flask import render_template,url_for,flash,redirect,request,abort
 from flask_blog import app
 from flask_blog.forms import RegistrationForm,LoginForm,UpdateAccountForm,PostForm
 from flask_blog.models import User,Post
@@ -109,10 +109,40 @@ def new_post():
         db.session.add(post)
         db.session.commit()
         return redirect(url_for('home'))
-    return render_template('create_post.html', title='New Post',form=form)
+    return render_template('create_post.html', title='New Post',form=form,legend="New Post")
 
 @app.route("/post/<int:post_id>")
 def post(post_id):
     post = Post.query.get_or_404(post_id)
     return render_template('post.html', title=post.title,post=post)
-    
+
+@app.route('/post/<int:post_id>/update',methods=['GET','POST'])
+@login_required
+def update_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    form = PostForm()
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash('Your post has been Updated!', 'success')
+        return redirect(url_for('post',post_id=post.id))
+    elif request.method == 'GET':
+        form.title.data = post.title
+        form.content.data = post.content
+    return render_template('create_post.html', title='Update Post'
+    ,form=form,legend="Update Post")
+
+
+@app.route("/post/<int:post_id>/delete",methods=['GET','POST'])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash('Your post has been deleted!', 'success')
+    return redirect(url_for('home'))
